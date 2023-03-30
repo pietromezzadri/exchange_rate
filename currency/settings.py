b2fa1fa272fefb0af44929ddc9d7bcd1638ab733
Pietro <pietromesadri@gmail.com>
2023-03-30 00:01:14 -0300

    settings change

--- currency/settings.py
+++ currency/settings.py
@@ -26,6 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+IS_HEROKU = "DYNO" in os.environ
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
